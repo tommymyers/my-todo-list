7fb635e17a781d8beda65908c3c01aa25346249a
Tommy Myers <tommy@tommymyers.dev>
2021-05-29 12:35:46 +0100

    Add a footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Tasks app, Tommy Myers 2021</em>
+    </div>
+  )
+}
+
+export default Footer
